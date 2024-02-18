dfcf24bc73129b96d5fd9e5934ae6a65fd27ca26
rishowrav <rishowrav@gmail.com>
2024-02-18 23:05:27 +0600

    add increment and decrement functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const classNames = document.getElementsByClassName("seat");
 let sum = 0;
+let incrementOfSeat = 0;
+let decrementOfSeat = 40;
 for (let className of classNames) {
   className.addEventListener("click", function (e) {
     // Set price list (price menu)
@@ -21,10 +23,16 @@ for (let className of classNames) {
     const seatPriceList = document.getElementById("seatPriceList");
     seatPriceList.appendChild(div);
 
-    // sum of price
+    // sum of price and show sum of total price
     const seatPrice = document.querySelector("#seatPriceList");
     sum = sum + parseInt(seatPrice.childNodes[0].childNodes[2].innerText);
     console.log(sum);
     document.getElementById("totalPrice").innerText = sum;
+
+    // increment and Decrement number of seat
+    incrementOfSeat++;
+    decrementOfSeat--;
+    document.getElementById("incrementOfSeat").innerText = incrementOfSeat;
+    document.getElementById("decrementOfSeat").innerText = decrementOfSeat;
   });
 }
