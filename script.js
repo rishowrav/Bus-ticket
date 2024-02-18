3dfff94d97dac786d6c3e141682cf74c414184ff
rishowrav <rishowrav@gmail.com>
2024-02-18 22:54:08 +0600

    add js file and dynamically add seat price list and sum of total price

---
+++ script.js
@@ -0,0 +1,30 @@
+const classNames = document.getElementsByClassName("seat");
+let sum = 0;
+for (let className of classNames) {
+  className.addEventListener("click", function (e) {
+    // Set price list (price menu)
+    const seatName = e.target.innerText;
+
+    const p1 = document.createElement("p");
+    p1.innerText = seatName;
+    const p2 = document.createElement("p");
+    p2.innerText = "Economoy";
+    const p3 = document.createElement("p");
+    p3.innerText = 550;
+    const div = document.createElement("div");
+    div.setAttribute("class", "flex justify-between text-gray-400");
+
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+
+    const seatPriceList = document.getElementById("seatPriceList");
+    seatPriceList.appendChild(div);
+
+    // sum of price
+    const seatPrice = document.querySelector("#seatPriceList");
+    sum = sum + parseInt(seatPrice.childNodes[0].childNodes[2].innerText);
+    console.log(sum);
+    document.getElementById("totalPrice").innerText = sum;
+  });
+}
