415bbf4d89405cca2e0550a6137ef93399e17fbb
rishowrav <rishowrav@gmail.com>
2024-02-19 17:48:18 +0600

    added validation functionality

--- script.js
+++ script.js
@@ -22,60 +22,61 @@ for (let className of classNames) {
 
     const seatPriceList = document.getElementById("seatPriceList");
 
-    // condition of seat plan (get 4 seats for one passenger)
-    if (incrementOfSeat < 4) {
-      seatPriceList.appendChild(div);
+    // double selected seat avoid (condition)
+    if (!e.target.classList.contains("selectedBusSeat")) {
+      if (incrementOfSeat < 4) {
+        // condition of seat plan (get 4 seats for one passenger)
+        seatPriceList.appendChild(div);
 
-      // double selected seat avoid (condition)
-      const selectSeat = e.target.innerText;
+        // add selected set color functionality
+        e.target.classList.add(
+          "bg-[#1DD100]",
+          "hover:bg-[#1DD100]",
+          "text-white",
+          "selectedBusSeat"
+        );
 
-      // add selected set color functionality
-      e.target.classList.add(
-        "bg-[#1DD100]",
-        "hover:bg-[#1DD100]",
-        "text-white"
-      );
+        // sum of price and show sum of total price
+        const seatPrice = document.querySelector("#seatPriceList");
+        sum = sum + parseInt(seatPrice.childNodes[0].childNodes[2].innerText);
+        document.getElementById("totalPrice").innerText = sum;
 
-      // sum of price and show sum of total price
-      const seatPrice = document.querySelector("#seatPriceList");
-      sum = sum + parseInt(seatPrice.childNodes[0].childNodes[2].innerText);
-      document.getElementById("totalPrice").innerText = sum;
+        // discount 15% and 20% condition
+        document
+          .getElementById("discountButton")
+          .addEventListener("click", function (e) {
+            const couponValue =
+              e.target.parentNode.childNodes[1].value.toUpperCase();
 
-      // discount 15% and 20% condition
-      document
-        .getElementById("discountButton")
-        .addEventListener("click", function (e) {
-          const couponValue =
-            e.target.parentNode.childNodes[1].value.toUpperCase();
+            if (couponValue === "NEW15") {
+              document.getElementById(
+                "totalPrice"
+              ).innerHTML = `<del>${sum}</del>`;
+              const discount15 = sum - (sum * 15) / 100;
+              document.getElementById("grandPrice").innerText = discount15;
+              console.log(discount15);
+              e.target.parentNode.classList.add("hidden");
+            } else if (couponValue === "COUPLE20") {
+              document.getElementById(
+                "totalPrice"
+              ).innerHTML = `<del>${sum}</del>`;
+              const discount20 = sum - (sum * 20) / 100;
+              document.getElementById("grandPrice").innerText = discount20;
+              console.log(discount20);
+              e.target.parentNode.classList.add("hidden");
+            } else {
+              e.target.parentNode.childNodes[1].classList.add("border-red-500");
+            }
+          });
 
-          if (couponValue === "NEW15") {
-            document.getElementById(
-              "totalPrice"
-            ).innerHTML = `<del>${sum}</del>`;
-            const discount15 = sum - (sum * 15) / 100;
-            document.getElementById("grandPrice").innerText = discount15;
-            console.log(discount15);
-            e.target.parentNode.classList.add("hidden");
-          } else if (couponValue === "COUPLE20") {
-            document.getElementById(
-              "totalPrice"
-            ).innerHTML = `<del>${sum}</del>`;
-            const discount20 = sum - (sum * 20) / 100;
-            document.getElementById("grandPrice").innerText = discount20;
-            console.log(discount20);
-            e.target.parentNode.classList.add("hidden");
-          } else {
-            e.target.parentNode.childNodes[1].classList.add("border-red-500");
-          }
-        });
-
-      // increment and Decrement number of seat
-      incrementOfSeat++;
-      decrementOfSeat--;
-      document.getElementById("incrementOfSeat").innerText = incrementOfSeat;
-      document.getElementById("decrementOfSeat").innerText = decrementOfSeat;
-    } else {
-      alert("You can selected max 4 seat");
+        // increment and Decrement number of seat
+        incrementOfSeat++;
+        decrementOfSeat--;
+        document.getElementById("incrementOfSeat").innerText = incrementOfSeat;
+        document.getElementById("decrementOfSeat").innerText = decrementOfSeat;
+      } else {
+        alert("You can select max 4 seats");
+      }
     }
   });
 }
